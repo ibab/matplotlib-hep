e7ef70afeba343f662bf9783fc8b7686c1473235
Igor Babuschkin <igor@babuschk.in>
2016-02-26 15:49:08 +0000

    Add plot_pull function

--- matplotlib_hep/__init__.py
+++ matplotlib_hep/__init__.py
@@ -5,7 +5,8 @@ import scipy.stats as stats
 import scipy as sp
 import logging
 
-__all__ = ['histpoints', 'calc_nbins']
+
+__all__ = ['histpoints', 'make_split', 'calc_nbins', 'plot_pull']
 
 def calc_nbins(x, maximum=150):
     n =  (max(x) - min(x)) / (2 * len(x)**(-1/3) * (np.percentile(x, 75) - np.percentile(x, 25)))
@@ -79,5 +80,59 @@ def histpoints(x, bins=None, xerr=None, yerr='gamma', normed=False, **kwargs):
 
     plt.errorbar(center, h, xerr=xerr, yerr=yerr, **kwargs)
 
-    return center, h, area
+    return center, (yerr[0], h, yerr[1]), area
+
+def make_split(ratio):
+    import matplotlib.pyplot as plt
+    from matplotlib.gridspec import GridSpec
+    from matplotlib.ticker import MaxNLocator
+    cax = plt.gca()
+    box = cax.get_position()
+    xmin, ymin = box.xmin, box.ymin
+    xmax, ymax = box.xmax, box.ymax
+    gs = GridSpec(2, 1, height_ratios=[ratio, 1 - ratio], left=xmin, right=xmax, bottom=ymin, top=ymax)
+    gs.update(hspace=0.12) 
+    #gs.update(hspace=0.0) 
+
+    ax = plt.subplot(gs[0])
+    plt.setp(ax.get_xticklabels(), visible=False)
+    bx = plt.subplot(gs[1], sharex=ax)
+
+    return ax, bx
+
+def plot_pull(data, func):
+
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from matplotlib.ticker import MaxNLocator
+
+    ax, bx = make_split(0.8)
+
+    plt.sca(ax)
+
+    x, y, norm = histpoints(data)
+
+    lower, upper = ax.get_xlim()
+
+    xs = np.linspace(lower, upper, 200)
+    plt.plot(xs, norm * func(xs), 'b-')
+
+    #plt.gca().yaxis.set_major_locator(MaxNLocator(prune='lower'))
+
+    plt.sca(bx)
+
+    resid = y[1] - norm * func(x)
+    err = np.zeros_like(resid)
+    err[resid >= 0] = y[0][resid >= 0]
+    err[resid < 0] = y[2][resid < 0]
+
+    pull = resid / err
+
+    plt.errorbar(x, pull, yerr=1, color='k', capsize=0, markersize=0, fmt='o', lw=2)
+    plt.ylim(-3, 3)
+    plt.axhline(0, color='k', alpha=0.5)
+
+    plt.sca(ax)
+
+    return ax, bx
 
