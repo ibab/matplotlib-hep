8fc55ae228512cdd4615e80a6d0e88ba03f6ea11
Igor Babuschkin <igor@babuschk.in>
2016-02-26 12:03:06 +0000

    Update setup.py description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='matplotlib-hep',
       version='0.1.0',
-      description='Module for making data analysis in High Energy Physics with Python more convenient',
+      description='A package for creating High Energy Physics plots more easily',
       url='http://github.com/ibab/matplotlib-hep',
       author='Igor Babuschkin',
       author_email='igor@babuschk.in',
